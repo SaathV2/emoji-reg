8a2803be2e37c92d95944615de7783a7134caebd
SaathV2 <87659400+SaathV2@users.noreply.github.com>
2022-05-24 18:20:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-prediction_1 = ""
-prediction_2 = ""
+prediction_1 = "";
+prediction_2 = "";
 
 Webcam.set({
     width:350,
@@ -22,3 +22,64 @@ function take_snapshot()
 console.log('ml5 version:', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9ZDx-bCQy/model.json' ,modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded')
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+function gotResults(error, results){
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = result[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = result[1].label;
+        prediction_1 = result[0].label;
+        prediction_1 = result[1].label;
+        speak();
+        if(result[0].label == "happy")
+        {
+            document.getElementById("update_emoji").innerHTML = "😀";
+        }
+        
+        if(result[0].label == "sad")
+        {
+            document.getElementById("update_emoji").innerHTML = "😒";
+        }
+
+        if(result[0].label == "angry")
+        {
+            document.getElementById("update_emoji").innerHTML = "😡";
+        }
+
+
+        if(result[1].label == "happy")
+        {
+            document.getElementById("update_emoji2").innerHTML = "😀";
+        }
+        
+        if(result[1].label == "sad")
+        {
+            document.getElementById("update_emoji2").innerHTML = "😒";
+        }
+
+        if(result[1].label == "angry")
+        {
+            document.getElementById("update_emoji2").innerHTML = "😡";
+        }       
+    }
+}
